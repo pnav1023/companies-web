05164f76f9411459f854236e5d72b6a9ed0ac059
Pranav Narahari <naraharipranav@gmail.com>
2024-09-26 22:08:45 -0500

    changed to hierachal, more formatting

--- ui.py
+++ ui.py
@@ -7,23 +7,40 @@ st.title("Web of companies")
 nodes = []
 edges = []
 
+topTen = 0
 for company in companies:
+    if topTen == 10: break
+
+    topTen += 1
     if company["id"] == "":
         nodes.append(Node(id=company["label"],
-                  label=company["label"]))
+                          color="red",
+                          label=company["label"]))
     else:   
         nodes.append(Node(id=company["id"],
-                  label=company["label"]))
-    
-# edges.append(Edge(source="Apple",
-#                   label="ceo",
-#                   target="ceoApple"))
+                          color="red",
+                          label=company["label"]))
+
+topTen = 0
+for ceo in ceos:
+    if topTen == 10: break
+
+    topTen += 1
+    if ceo["companyTicker"] == "":
+        nodes.append(Node(id=ceo["label"],
+                  label=ceo["label"]))
+    else:
+        nodes.append(Node(id=ceo["id"],
+                  label=ceo["label"]))
+        edges.append(Edge(source=ceo["companyTicker"],
+                          label="ceo",
+                          target=ceo["id"]))
 
 config = Config(width=750,
                 height=950,
                 directed=True, 
                 physics=True, 
-                hierarchical=False,
+                hierarchical=True,
                 # **kwargs
                 )
 
